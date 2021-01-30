10313a40693da16656b35e31f3849b91fd0c2201
Emil <eeremiev@chequemotiva.com>
2021-01-30 05:12:17 +0100

    add migrations and route

---
+++ app/Models/Book.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'rating',
+    ];
+
+    public function author()
+    {
+        return $this->belongsTo(Author::class);
+    }
+}
